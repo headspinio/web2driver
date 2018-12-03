da1f41b45ac44f41ed069a37edc0cfd916aceea9
Jonathan Lipps <jlipps@gmail.com>
2018-12-03 10:17:36 -0800

    ensure we export for commonjs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import Web2Driver from './driver';
 
+export { Web2Driver };
 export default Web2Driver;
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
   entry: ['@babel/polyfill', './src/index.js'],
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs',
   },
   node: {
     fs: "empty",
